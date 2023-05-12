fab57e4285f238d5eacbaa0d26d3a6f76eec048b
cbartek <bartek.ch15@gmail.com>
2023-05-12 23:17:44 +0200

    Add group entity

---
+++ src/main/java/pl/cholewa/sharethebills/group/Group.java
@@ -0,0 +1,30 @@
+package pl.cholewa.sharethebills.group;
+
+import lombok.*;
+import pl.cholewa.sharethebills.bill.Bill;
+import pl.cholewa.sharethebills.user.User;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "groups")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Group {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @ManyToMany
+    private List<User> users = new ArrayList<>();
+    @OneToMany(mappedBy = "group")
+    private List<Bill> bills = new ArrayList<>();
+
+
+}
