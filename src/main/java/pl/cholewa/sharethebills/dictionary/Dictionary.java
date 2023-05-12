9f0045030e33bdb7fe89e260dff186be7e7ebc48
cbartek <bartek.ch15@gmail.com>
2023-05-12 22:22:19 +0200

    Add dictionary entity

---
+++ src/main/java/pl/cholewa/sharethebills/dictionary/Dictionary.java
@@ -0,0 +1,27 @@
+package pl.cholewa.sharethebills.dictionary;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="dictionaries")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Dictionary {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String description;
+
+    @OneToMany(mappedBy = "dictionary")
+    private List<DictionaryValue> dictionaryValueList = new ArrayList<>();
+}
