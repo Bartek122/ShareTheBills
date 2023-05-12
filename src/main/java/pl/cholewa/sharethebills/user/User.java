3d0b0672e48fb4dc12c10beee1dfc9505e6885d8
cbartek <bartek.ch15@gmail.com>
2023-05-12 22:12:19 +0200

    Add user entity

---
+++ src/main/java/pl/cholewa/sharethebills/user/User.java
@@ -0,0 +1,30 @@
+package pl.cholewa.sharethebills.user;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="Users")
+@Getter @Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+
+    @OneToMany(mappedBy = "userId")
+    private List<UserAttribute> userAttributesList = new ArrayList<>();
+
+}
