1cbf5963f47fbf90de0dd1bec86e27c25c3ca58b
cbartek <bartek.ch15@gmail.com>
2023-05-12 21:44:23 +0200

    first commit

---
+++ src/main/java/pl/cholewa/sharethebills/ShareTheBillsApplication.java
@@ -0,0 +1,13 @@
+package pl.cholewa.sharethebills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareTheBillsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareTheBillsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/cholewa/sharethebills/ShareTheBillsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.cholewa.sharethebills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareTheBillsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
