3f48c7b3b1c20da678a5500c5ecab2fda08e5816
cbartek <bartek.ch15@gmail.com>
2023-05-25 18:27:07 +0200

    Refactor Bill entity and add relation with User and BillDetail

--- src/main/java/pl/cholewa/sharethebills/bill/Bill.java
+++ src/main/java/pl/cholewa/sharethebills/bill/Bill.java
@@ -1,10 +1,13 @@
 package pl.cholewa.sharethebills.bill;
 
 import lombok.*;
+import pl.cholewa.sharethebills.billDetail.BillDetail;
 import pl.cholewa.sharethebills.group.Group;
+import pl.cholewa.sharethebills.user.User;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.util.List;
 
 @Entity
 @Table(name = "bills")
@@ -13,14 +16,17 @@ import java.math.BigDecimal;
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-
+@Builder
 public class Bill {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private BigDecimal price;
     private String description;
+    private BigDecimal sumPrice;
+    @ManyToOne
+    private User payer;
     @ManyToOne
     private Group group;
-
+    @OneToMany(mappedBy = "masterBill")
+    private List<BillDetail> billDetails;
 }
