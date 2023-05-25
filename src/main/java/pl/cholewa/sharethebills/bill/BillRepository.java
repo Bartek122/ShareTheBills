8588c7f6933c4e652b60b67fc70de46eca612da3
cbartek <bartek.ch15@gmail.com>
2023-05-25 18:32:04 +0200

    Add findAllByPayer in BillRepository

--- src/main/java/pl/cholewa/sharethebills/bill/BillRepository.java
+++ src/main/java/pl/cholewa/sharethebills/bill/BillRepository.java
@@ -1,7 +1,12 @@
 package pl.cholewa.sharethebills.bill;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import pl.cholewa.sharethebills.user.User;
+
+import java.util.List;
 
 public interface BillRepository extends JpaRepository<Bill, Long> {
+
+    List<Bill> findAllByPayer(User payer);
 
 }
