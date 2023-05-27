d2f22861189c892f344cba5a4e49b89ab990ef61
cbartek <bartek.ch15@gmail.com>
2023-05-27 03:03:04 +0200

    Remove CreateBillDetailRequest

--- src/main/java/pl/cholewa/sharethebills/billDetail/CreateBillDetailRequest.java
+++
@@ -1,9 +0,0 @@
-package pl.cholewa.sharethebills.billDetail;
-
-import javax.validation.constraints.DecimalMin;
-import java.math.BigDecimal;
-
-public record CreateBillDetailRequest(
-        @DecimalMin(value="0.01") BigDecimal price
-) {
-}
